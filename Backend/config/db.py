b293061b3d2c6b560d17de2e8cb449e3d7624987
JesusAlejandroCabreraGarcia <95956764+JesusAlejandroCabreraGarcia@users.noreply.github.com>
2024-07-19 16:56:38 -0600

    Update db.py

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@localhost:3307/test'  #Conexión local
-
+#SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@localhost:3307/test'  #Conexión local
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://avnadmin:AVNS_WWwmfvuplg6Muz-kyNC@mysql-20401d83-alejandroxlllc-f203.e.aivencloud.com:28124/defaultdb'
 #  Conexión local
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
