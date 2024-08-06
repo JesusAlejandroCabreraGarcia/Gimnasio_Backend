20ad01da085e6204c1c8c00ac12d1531fe0ee765
alejandro <alejandroxlllc@gmail.com>
2024-08-06 10:56:28 -0600

    agregando el horario

--- Backend/app.py
+++ Backend/app.py
@@ -3,10 +3,12 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.usersrols import userrol
+from routes.schedule import schedule
 
 app = FastAPI()
 app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
+app.include_router(schedule)
 print("Hola bienvenido a mi backend")
